7f90c012f3f6633e326361248c99bbcc7cebaaa9
KONi-7 <koga.amrr.n@outlook.jp>
2026-07-13 15:23:53 +0900

    ci:configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://KONi-7.github.io",
+    base: "/Profile",
+});
